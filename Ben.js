d3c1b95f5fcbcee6c8510d9c38561398fa8b46a6
Chan Shu Leung <kinagiyuki@gmail.com>
2019-05-04 00:26:59 +0800

    Update beat play implementation ver1.2

--- Ben.js
+++ Ben.js
@@ -1,9 +1,15 @@
 const timeoutTime = 500;
+const MAX_BEAT = 16;
+const DRUMS_LENGTH = 6;
+var isLoop = false;
+var isPlay = false;          // Is-Loop flag
+var matrix = [];           // Boolean data array
+var beat = 0;               // Store the time domain iterator of matrix
+
 // Drum instrument play back function
 // with NoteOff
 //  |   (drumType, amplitude)
-
-function playDrum(drumType, amplitude) {
+function playDrum(drumType, panning, amplitude) {
     // Prompt the amplitude
     amplitude = (amplitude > MAX_AMPLITUDE) ? MAX_AMPLITUDE : (amplitude < MIN_AMPLITUDE) ? MIN_AMPLITUDE : amplitude;
     // Drum chaanel  = 9
@@ -15,30 +21,45 @@ function playDrum(drumType, amplitude) {
 }
 
 // How to play?
-// Idea 1
-function playIdeaOne() {
-    // Loop time domain first?
-    //  matrix -
-    //  |   t: 1-way array store all drum instruments flag
-    //  |   t, i: each instrument ON flag in time t
-    var timeLength = matrix.length;
-    for (var t = 0; t < timeLength; t++) {
-        var instrumentLength = matrix[t].length;
-        for (var i = 0; i < instrumentLength; i++) {
-            if (matrix[t][i]) {
-                playDrum(instrument[i], amplitude);
-            }
+// Play one beat[t] for each drum instrument
+function playOneBeat(spb) {
+    // Check if 'beat' is larger than MAX_BEAT when looping
+    if (beat >= MAX_BEAT) {
+        beat = 0;
+    }
+
+    // Play each instrument in one beat
+    for (var i = 0; i < DRUMS_LENGTH; i++) {
+        if (matrix[beat][i]) {
+            playDrum(instrument[i], pannings[i], amplitude);
         }
+    }
+
+    // Move to next beat
+    beat++;
+
+    if (isPlay && (beat < MAX_BEAT || isLoop)) {
+        setTimeout(function () {
+            playOneBeat(spb);
+        }, spb);
     }
 }
 
-// Function after "Play" button is pressed
+// Function after "PLAY" button is pressed
 function playBtnPressed() {
     // Grap those variables from front end
-    const tempo = 0;        // Tempo
-    const isLoop = true;    // Is-Loop flag
-    const matrix = [];      // Boolean data array
+    const tempo = 80;    // Tempo: Need to grap from front-end
+    matrix = [];        // Grapped front-end info
+    isPlay = true;
 
-    // Calculate the how many second for 1 beat
-    const secondPerBeat = 1.0 / (tempo / 60);
+    // Calculate the how many second for 1 beat (Second per Beat)
+    const spb = 1.0 / (tempo / 60.0);
+    // Auto play full beats
+    playOneBeat(spb);
+}
+
+// Function after "STOP" button is pressed
+function stopBtnPressed() {
+    beat = 0;
+    isPlay = false;
 }
