1a6a489ebb529fcd72c31f14f6073452f7282744
Adam Feuer <adamfeuer001@gmail.com>
2019-05-05 21:18:01 +0800

    mostly working, put together html and js

--- Ben.js
+++ Ben.js
@@ -3,9 +3,12 @@ const MAX_BEAT = 16;
 const DRUMS_LENGTH = 6;
 var isLoop = false;
 var isPlay = false;         // Is-Loop flag
-var matrix = [];            // Boolean data array
+//var matrix = [];            // Boolean data array
 var instrument = [];        // Channel 10th with key note (35~81)
 var beat = 0;               // Store the time domain iterator of matrix
+
+var MIN_AMPLITUDE = 0;
+var MAX_AMPLITUDE = 127;
 
 // Drum instrument play back function
 // with NoteOff
@@ -14,10 +17,10 @@ function playDrum(drumType, panning, amplitude) {
     // Prompt the amplitude
     amplitude = (amplitude > MAX_AMPLITUDE) ? MAX_AMPLITUDE : (amplitude < MIN_AMPLITUDE) ? MIN_AMPLITUDE : amplitude;
     // Drum chaanel  = 9
-    MIDI.noteOn(9, drumType, amplitude);
+    MIDI.noteOn(0, drumType, amplitude);
     // Note off the drum
     setTimeout(function functionName() {
-        MIDI.noteOff(9, drumType);
+        MIDI.noteOff(0, drumType);
     }, timeoutTime);
 }
 
@@ -28,17 +31,25 @@ function playOneBeat(spb) {
     if (beat >= MAX_BEAT) {
         beat = 0;
     }
+    //CODE to move the step
+    $(".cellstep").removeClass('active');
+    $("#step").find("th").eq(beat+1).find("button").addClass('active');
 
     // Play each instrument in one beat
     for (var i = 0; i < DRUMS_LENGTH; i++) {
-        if (matrix[beat][i]) {
-            playDrum(instrument[i], pannings[i], amplitude);
+        if (matrix[i][beat]) {
+            //alert("in here" + " beat: "+ beat + "i: " + i); //WORKING NOW
+            var volume = $('#volume'+i).val(); //get infomation for that instrument
+            var panning = $('#panning'+i).val();
+            volume=127;
+            var instrument = $('#track'+i).data("num");
+            playDrum(instrument, panning, volume); 
+            //playDrum(instrument[i], pannings[i], amplitude); //COMMENTED OUT FOR THE MOMENT AS DON'T HAVE ANY VALS
         }
     }
 
     // Move to next beat
     beat++;
-
     if (isPlay && (beat < MAX_BEAT || isLoop)) {
         setTimeout(function () {
             playOneBeat(spb);
@@ -49,8 +60,9 @@ function playOneBeat(spb) {
 // Function after "PLAY" button is pressed
 function playBtnPressed() {
     // Grap those variables from front end
-    const tempo = 80;    // Tempo: Need to grap from front-end
-    matrix = [];        // Grapped front-end info
+    //const tempo = 80;    // Tempo: Need to grap from front-end
+    const tempo = $('#pattern_tempo').val();
+    //matrix = [];        // Grapped front-end info
     isPlay = true;
 
     // Calculate the how many second for 1 beat (Second per Beat)
--- lab01.js
+++ lab01.js
@@ -1,110 +1,9 @@
-// // The pitch of the currently playing note
-// var this_pitch;
-
-// // The MIDI pitch number for the first (left) keyboard key
-// var lowest_pitch = 60;
-
-// var MIN_PITCH = 21;
-// var MAX_PITCH = 108;
-
-// var MIN_AMPLITUDE = 0;
-// var MAX_AMPLITUDE = 127;
-
-// var current_chord_type;
-
-// function handlePianoKeyPress(evt) {
-
-// 	current_chord_type = $(":radio[name=play-mode]:checked").val();
-
-//     var this_key, this_amplitude;
-
-//     // Determine which piano key has been pressed.
-//     // 'evt.target' tells us exactly which item triggered this function.
-
-//     // The piano key number is taken from the 'data-piano-key-number' attribute of each button.
-//     // The piano key number is a value in the range 0 to 23 inclusive.
-//     this_key = $(evt.target).data("pianoKeyNumber");
-
-//     lowest_pitch = parseInt($("#pitch").val());
-
-//     this_pitch = lowest_pitch + parseInt(this_key);
-
-//     // Extract the amplitude value from the slider
-//     this_amplitude = $("#amplitude").val();
-
-//     // Convert the string into actual values
-//     this_amplitude = parseInt(this_amplitude);
-
-//     // Use the two numbers to start a MIDI note
-//     MIDI.noteOn(0, this_pitch, this_amplitude);
-
-//     //
-//     // You need to handle the chord mode here
-//     //
-
-
-//     switch(current_chord_type){
-//     	case "major":
-// 	    	if((this_pitch+4 <= MAX_PITCH) && (this_pitch+4 >= MIN_PITCH)){
-// 	    		 MIDI.noteOn(0, this_pitch+4, this_amplitude);
-// 	    	}
-// 	    	if((this_pitch+7 <= MAX_PITCH) && (this_pitch+7 >= MIN_PITCH)){
-// 	    		 MIDI.noteOn(0, this_pitch+7, this_amplitude);
-// 	    	}
-// 	    	break;
-//     	case "minor":
-// 	    	if((this_pitch+3 <= MAX_PITCH) && (this_pitch+3 >= MIN_PITCH)){
-// 	    		 MIDI.noteOn(0, this_pitch+3, this_amplitude);
-// 	    	}
-// 	    	if((this_pitch+7 <= MAX_PITCH) && (this_pitch+7 >= MIN_PITCH)){
-// 	    		 MIDI.noteOn(0, this_pitch+7, this_amplitude);
-// 	    	}
-//     		break;
-//     }
-
-  
-//     // Show a simple message in the console
-//     console.log("Key press event for key " + this_pitch + "!");
-// }
-
-// function handlePianoKeyRelease(evt) {
-//     // Send the note off message to match the pitch of the current note on event
-
-//     MIDI.noteOff(0, this_pitch); 
-
-//     //
-//     // You need to handle the chord mode here
-//     //
-
-//     switch(current_chord_type){
-//     	case "major":
-// 	    	if((this_pitch+4 <= MAX_PITCH) && (this_pitch+4 >= MIN_PITCH)){
-// 	    		 MIDI.noteOff(0, this_pitch+4);
-// 	    	}
-// 	    	if((this_pitch+7 <= MAX_PITCH) && (this_pitch+7 >= MIN_PITCH)){
-// 	    		 MIDI.noteOff(0, this_pitch+7);
-// 	    	}
-// 	    	break;
-//     	case "minor":
-// 	    	if((this_pitch+3 <= MAX_PITCH) && (this_pitch+3 >= MIN_PITCH)){
-// 	    		 MIDI.noteOff(0, this_pitch+3);
-// 	    	}
-// 	    	if((this_pitch+7 <= MAX_PITCH) && (this_pitch+7 >= MIN_PITCH)){
-// 	    		 MIDI.noteOff(0, this_pitch+7);
-// 	    	}
-//     		break;
-//     }
-
-
-//     // Show a simple message in the console
-//     console.log("Key release event for key " + this_pitch + "!");
-// }
-
+var matrix;
 $(document).ready(function() {
     var isplaying=0;
     var num_tracks=6;
     var length = 16;
-    var matrix = new Array(num_tracks);
+    matrix = new Array(num_tracks);
     for(var col=0; col< num_tracks; col++){
             matrix[col] = new Array(length);
     }
@@ -116,6 +15,7 @@ $(document).ready(function() {
     }
    //MATRIX GOES [ROW][COL]
     //debugger;
+    
 
         $(".cell").click(function() {
             //get index of the cell
@@ -130,7 +30,6 @@ $(document).ready(function() {
             }
             //alert("col: " +col + " row: " + row + " value: " + value);
             matrix[row][col]=value;
-            debugger;
 
         });
 
@@ -138,7 +37,7 @@ $(document).ready(function() {
             isplaying=(isplaying+1)%2;
             if (isplaying==1)
                 {
-                    //alert("play");
+                    playBtnPressed();
                     $(this).html("Stop");
 
             // $(".cellstep").removeClass('active'); - HOW TO ACTIVATE A DECATIVE THE STEP
@@ -147,52 +46,54 @@ $(document).ready(function() {
             else
                 {
                     //alert("stop");
+                    stopBtnPressed();
                     $(this).html("Play");
                 }
               }
            );
 
-    // MIDI.loadPlugin({
-    //     soundfontUrl: "./midi-js/soundfont/",
-    //     instruments: [
-    //         "trumpet"
-    //         //
-    //         // You can optionally list the instruments here to preload the
-    //         // instruments; alternatively, each instrument will be loaded
-    //         // when you send the program change event
-    //         //
-    //     ],
-    //     onprogress: function(state, progress) {
-    //         console.log(state, progress);
-    //     },
-    //     onsuccess: function() {
-    //         // Resuming the AudioContext when there is user interaction
-    //         $("body").click(function() {
-    //             if (MIDI.getContext().state != "running") {
-    //                 MIDI.getContext().resume().then(function() {
-    //                     console.log("Audio Context is resumed!");
-    //                 });
-    //             }
-    //         });
 
-    //         // At this point the MIDI system is ready to be used
-    //         MIDI.setVolume(0, 127);     // Set the volume level
-    //         MIDI.programChange(0, 56);  // Use the General MIDI 'trumpet' number
+    MIDI.loadPlugin({
+        soundfontUrl: "./midi-js/soundfont/",
+        instruments: [
+            "drum"
+            //
+            // You can optionally list the instruments here to preload the
+            // instruments; alternatively, each instrument will be loaded
+            // when you send the program change event
+            //
+        ],
+        onprogress: function(state, progress) {
+            console.log(state, progress);
+        },
+        onsuccess: function() {
+            // Resuming the AudioContext when there is user interaction
+            $("body").click(function() {
+                if (MIDI.getContext().state != "running") {
+                    MIDI.getContext().resume().then(function() {
+                        console.log("Audio Context is resumed!");
+                    });
+                }
+            });
 
-    //         // Set up the event handlers for all the buttons
-    //         $("button").on("mousedown", handlePianoKeyPress);
-    //         $("button").on("mouseup", handlePianoKeyRelease);
+            // At this point the MIDI system is ready to be used
+            MIDI.setVolume(0, 127);     // Set the volume level
+            MIDI.programChange(0, 0);  // Use the General MIDI 'trumpet' number
 
-    //         //
-    //         // You probably need to set up an event for your instrument change
-    //         //
-    //         $("#instrument").change(function(){
-    //         	var instrumentNumber = parseInt($("#instrument").val());
-    //         	//console.log(instrumentNumber);
-    //         	MIDI.programChange(0, instrumentNumber);
-    //         });
+            // Set up the event handlers for all the buttons
+            //$("button").on("mousedown", handlePianoKeyPress);
+            //$("button").on("mouseup", handlePianoKeyRelease);
+
+            //
+            // You probably need to set up an event for your instrument change
+            //
+            // $("#instrument").change(function(){
+            // 	var instrumentNumber = parseInt($("#instrument").val());
+            // 	//console.log(instrumentNumber);
+            // 	MIDI.programChange(0, instrumentNumber);
+            // });
 
 
-    //     }
-    // });
+        }
+    });
 });
